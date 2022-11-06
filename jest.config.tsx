13541dd81e584dcbe2788eec4f5ebbd3b8fe6a59
PauloCazuza <prlopes_@hotmail.com>
2022-11-06 11:06:59 -0300

    add config tests to async storage

--- jest.config.tsx
+++ jest.config.tsx
@@ -1,22 +1,26 @@
 import type { Config } from '@jest/types';
 import { defaults } from 'jest-config';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
 // Sync object
 
 const esModules = ['lodash-es', 'axios'].join('|')
 
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
 const config: Config.InitialOptions = {
-    verbose: true,
-    transform: {
-        "\\.[jt]sx?$": "babel-jest",
-    },
-    moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
-    transformIgnorePatterns: ["node_modules/(?!axios)"],
-    presets: [
+  verbose: true,
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+  },
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+  transformIgnorePatterns: ["node_modules/(?!axios)"],
+  presets: [
     [
       '@babel/preset-env',
-        targets: {
-          node: 'current'
-        }
+      targets: {
+        node: 'current'
+      }
     ]
 };
 export default config;
